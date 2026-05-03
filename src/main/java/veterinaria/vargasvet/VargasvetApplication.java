4924bf5b49e52713aac09e557ec511e3dcbc4821
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-05-03 00:01:27 -0500

    feat: estructura inicial del sistema veterinario

---
+++ src/main/java/veterinaria/vargasvet/VargasvetApplication.java
@@ -0,0 +1,13 @@
+package veterinaria.vargasvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VargasvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VargasvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/veterinaria/vargasvet/VargasvetApplicationTests.java
@@ -0,0 +1,13 @@
+package veterinaria.vargasvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VargasvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
