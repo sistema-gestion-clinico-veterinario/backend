f2f31a5061cef42590f0c94c70bd8726022cc658
SaavedraSebastian <ssaavedraa4@upao.edu.pe>
2026-05-22 17:05:04 -0500

    fix: Correcion menu v1

--- src/main/java/veterinaria/vargasvet/domain/entity/Vista.java
+++ src/main/java/veterinaria/vargasvet/domain/entity/Vista.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -37,4 +39,13 @@ public class Vista {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ventana_id", nullable = true)
     private Ventana ventana;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", nullable = true)
+    @ToString.Exclude
+    private Vista parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Vista> hijos = new ArrayList<>();
 }
