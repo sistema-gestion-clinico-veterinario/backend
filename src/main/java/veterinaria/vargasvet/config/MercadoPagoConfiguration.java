aaed272e8b4b2e73de356dff28d503a9de0c00f5
Diogho Matteo Baylon Toledo <dbaylont1@upao.edu.pe>
2026-06-14 12:26:30 -0500

    correcion de espacio para subida de archivos

--- src/main/java/veterinaria/vargasvet/config/MercadoPagoConfiguration.java
+++ src/main/java/veterinaria/vargasvet/config/MercadoPagoConfiguration.java
@@ -2,11 +2,12 @@ package veterinaria.vargasvet.config;
 
 import com.mercadopago.MercadoPagoConfig;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
+@RequiredArgsConstructor
 @Configuration
 public class MercadoPagoConfiguration {
 
@@ -18,8 +19,4 @@ public class MercadoPagoConfiguration {
         MercadoPagoConfig.setAccessToken(accessToken);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
